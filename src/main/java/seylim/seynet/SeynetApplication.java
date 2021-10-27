87e39cde5def2210ebaa08d5bda739153291479c
Emre Kaan Seylim <seylimemre@gmail.com>
2021-10-27 12:39:17 +0300

    First commit, empty project.

---
+++ src/main/java/seylim/seynet/SeynetApplication.java
@@ -0,0 +1,13 @@
+package seylim.seynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeynetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeynetApplication.class, args);
+    }
+
+}
