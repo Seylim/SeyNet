7d7d6d486cee3930624df64e12cd8110ed7868bb
Emre Kaan Seylim <seylimemre@gmail.com>
2021-11-10 18:29:37 +0300

    Candidate class created.

---
+++ src/main/java/seylim/seynet/entities/concretes/Candidate.java
@@ -0,0 +1,40 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "cadidates")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Candidate extends User{
+
+    @Id
+    @Column(name = "candidateId")
+    private int candidateId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "national_identity_number")
+    private String nationalIdNumber;
+
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    //JsonIgnore
+    //@OneToMany(mappedBy="candidate")
+    //private List<Resume> resumes;
+}
