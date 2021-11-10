d7511251f0c0203e9434b3c57d19c8ba486c3b23
Emre Kaan Seylim <seylimemre@gmail.com>
2021-11-10 20:20:37 +0300

    Candidate class created.

--- src/main/java/seylim/seynet/entities/concretes/Candidate.java
+++ src/main/java/seylim/seynet/entities/concretes/Candidate.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -19,6 +16,7 @@ import javax.persistence.Table;
 public class Candidate extends User{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "candidateId")
     private int candidateId;
 
