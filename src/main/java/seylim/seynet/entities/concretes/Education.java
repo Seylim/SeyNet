0603a855782c82772ad5ef83db1835923d8ad7c0
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-08 20:16:06 +0300

    Education class added.

---
+++ src/main/java/seylim/seynet/entities/concretes/Education.java
@@ -0,0 +1,42 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "educations")
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name_of_educational_institution")
+    private String nameOfEducationalInstitution;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "degree")
+    private String degree;
+
+    @Column(name = "starting_date")
+    private LocalDate startingDate;
+
+    @Column(name = "graduation_date")
+    private LocalDate graduationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
+
+}
