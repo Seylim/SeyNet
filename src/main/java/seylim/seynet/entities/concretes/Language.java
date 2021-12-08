588429db39f054c70bf49b3dd7ae40ef9eef3f72
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-08 20:16:30 +0300

    Language class added.

---
+++ src/main/java/seylim/seynet/entities/concretes/Language.java
@@ -0,0 +1,29 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "languages")
+public class Language {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "lenguage")
+    private String language;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
+}
