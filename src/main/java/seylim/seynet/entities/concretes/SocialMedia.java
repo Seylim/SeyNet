1eba4f3d373cc61b3793e772314c8287e4fd3572
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-08 20:16:44 +0300

    SocialMedia class added.

---
+++ src/main/java/seylim/seynet/entities/concretes/SocialMedia.java
@@ -0,0 +1,32 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "social_medias")
+public class SocialMedia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "github")
+    private String github;
+
+    @Column(name = "linkedin")
+    private String linkedIn;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
+}
