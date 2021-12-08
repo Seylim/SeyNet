7e47631c29eefd5f873a64d702545cd8400afa80
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-08 20:15:41 +0300

    LanguageLevel class added.

---
+++ src/main/java/seylim/seynet/entities/concretes/LanguageLevel.java
@@ -0,0 +1,29 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "language_levels")
+public class LanguageLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "level")
+    private String level;
+
+    @OneToOne
+    @JoinColumn(name = "language_id")
+    private Language language;
+}
