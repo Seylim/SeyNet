362d9978904c333009de0d6b90977d20f5c82c56
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-12 14:09:52 +0300

    Created functions in the candidateController

--- src/main/java/seylim/seynet/api/controllers/CandidateController.java
+++ src/main/java/seylim/seynet/api/controllers/CandidateController.java
@@ -3,10 +3,11 @@ package seylim.seynet.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import seylim.seynet.business.abstracts.CandidateService;
-import seylim.seynet.core.utilities.Messages;
+import seylim.seynet.core.utilities.DataResult;
 import seylim.seynet.core.utilities.Result;
-import seylim.seynet.core.utilities.SuccessResult;
 import seylim.seynet.entities.concretes.Candidate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/candidates")
@@ -24,8 +25,28 @@ public class CandidateController {
         return this.candidateService.add(candidate);
     }
 
+    @PutMapping("/update")
+    public Result update(@RequestBody Candidate candidate){
+        return this.candidateService.update(candidate);
+    }
+
     @DeleteMapping("/delete")
     public Result delete(int id) {
         return this.candidateService.delete(id);
+    }
+
+    @GetMapping("/getall")
+    public DataResult<List<Candidate>> getAll(){
+        return this.candidateService.getAll();
+    }
+
+    @GetMapping("/getbyid")
+    public DataResult<Candidate> getById(int id){
+        return this.candidateService.getById(id);
+    }
+
+    @GetMapping("/getbynationalidnumber")
+    public DataResult<Candidate> getByNationalIdNumber(int nationalIdNumber){
+        return this.candidateService.getByNationalIdNumber(nationalIdNumber);
     }
 }
