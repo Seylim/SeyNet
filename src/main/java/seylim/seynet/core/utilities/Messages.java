2a98ac7aa8344124a797f9c1199e0e9f044787be
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-12 14:15:33 +0300

    A static class named messages has been created

--- src/main/java/seylim/seynet/core/utilities/Messages.java
+++ src/main/java/seylim/seynet/core/utilities/Messages.java
@@ -1,6 +1,9 @@
 package seylim.seynet.core.utilities;
 
 public class Messages {
-    public static String successAdd = "added the successful";
-    public static String successDelete = "Deleted the successful";
+    public static String successAdd = "Added the Successful";
+    public static String successDelete = "Deleted the Successful";
+    public static String successUpdate = "Uptaded the Successful";
+    public static String dataListed = "Data is Listed";
+
 }
