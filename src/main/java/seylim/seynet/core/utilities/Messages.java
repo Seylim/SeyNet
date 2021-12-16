4fd43c5eba2e8fec74b9bd88221b1582d84b261d
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-16 15:37:34 +0300

    added messages.

--- src/main/java/seylim/seynet/core/utilities/Messages.java
+++ src/main/java/seylim/seynet/core/utilities/Messages.java
@@ -6,4 +6,21 @@ public class Messages {
     public static String successUpdate = "Uptaded the Successful";
     public static String dataListed = "Data is Listed";
 
+
+    public static String firstNameNotNull = "First Name Cannot Be Left Blank";
+    public static String lastNameNotNull = "Last Name Cannot Be Left Blank";
+    public static String nationalIdNotNull = "National Id Cannot Be Left Blank";
+    public static String yearOfBirtNotNull = "Year Of Birth Cannot Be Left Blank";
+    public static String emailNotNull = "Email Cannot Be Left Blank";
+    public static String passwordNotNull = "Password Cannot Be Left Blank";
+
+    public static String companyNameNotNull = "Company Name Cannot Be Left Blank";
+    public static String webSiteNotNull = "Company Name Cannot Be Left Blank";
+    public static String phoneNumberNotNull = "Phone Number Cannot Be Left Blank";
+
+
+    public static String registeredEmail = "Email is Registered to The System";
+    public static String registeredID = "This ID Number is Registered to The System";
+    public static String registrationIsSuccessful = "Registration is Successful";
+    public static String informationIsIncorrect = "Your Information is Incorrect";
 }
