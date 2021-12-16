8aa3d1e2866902793a1343f52c43fb00dc0b80ab
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-16 23:04:01 +0300

    BusinessRules class has been created.

---
+++ src/main/java/seylim/seynet/core/business/BusinessRules.java
@@ -0,0 +1,18 @@
+package seylim.seynet.core.business;
+
+import seylim.seynet.core.utilities.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessRules {
+    public static Result[] run(Result... logics){
+        List<Result> results = new ArrayList<Result>();
+        for(Result logic:logics){
+            if (!logic.isSuccess()){
+                results.add(logic);
+            }
+        }
+        return results.toArray(new Result[results.size()]);
+    }
+}
