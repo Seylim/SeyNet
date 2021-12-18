c27bfe1124b4fe9347acda007531e28b99f367a3
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-18 16:25:00 +0300

    register and login functions has been created for employer.

--- src/main/java/seylim/seynet/business/concretes/EmployerManager.java
+++ src/main/java/seylim/seynet/business/concretes/EmployerManager.java
@@ -3,8 +3,10 @@ package seylim.seynet.business.concretes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import seylim.seynet.business.abstracts.EmployerService;
+import seylim.seynet.business.abstracts.EmployerValidService;
 import seylim.seynet.core.utilities.*;
 import seylim.seynet.dataAccess.abstracts.EmployerDao;
+import seylim.seynet.entities.concretes.Candidate;
 import seylim.seynet.entities.concretes.Employer;
 
 import java.util.List;
@@ -13,10 +15,38 @@ import java.util.List;
 public class EmployerManager implements EmployerService {
 
     private EmployerDao employerDao;
+    private EmployerValidService employerValidService;
 
     @Autowired
-    public EmployerManager(EmployerDao employerDao){
+    public EmployerManager(EmployerDao employerDao, EmployerValidService employerValidService){
         this.employerDao = employerDao;
+        this.employerValidService = employerValidService;
+    }
+
+    @Override
+    public Result register(Employer employer) throws Exception {
+        List<Employer> employers = employerDao.findAll();
+        if (checkIfUserExist(employers, employer).isSuccess()){
+            if (this.employerValidService.validate(employer).isSuccess()){
+                this.employerDao.save(employer);
+                return new SuccessResult(Messages.registrationIsSuccessful);
+            }
+            return new ErrorResult(Messages.informationIsIncorrect);
+        }
+        return checkIfUserExist(employers, employer);
+    }
+
+    @Override
+    public Result login(String eMail, String passwordAgain) throws Exception {
+        List<Employer> employers = this.employerDao.findAll();
+        for (Employer dbEmployer: employers){
+            if (dbEmployer.getEmail().equals(eMail)){
+                if (dbEmployer.getPassword().equals(passwordAgain)){
+                    return new SuccessResult(Messages.loginIsSuccessful);
+                }
+            }
+        }
+        return new ErrorResult(Messages.informationIsIncorrect);
     }
 
     @Override
@@ -45,5 +75,14 @@ public class EmployerManager implements EmployerService {
     @Override
     public DataResult<Employer> getById(int id) {
         return new SuccessDataResult<Employer>(this.employerDao.getById(id), Messages.dataListed);
+    }
+
+    private Result checkIfUserExist(List<Employer> employers, Employer checkEmployer){
+        for (Employer employer : employers){
+            if (employer.getEmail().equals(checkEmployer.getEmail())){
+                return new ErrorResult(Messages.registeredEmail);
+            }
+        }
+        return new SuccessResult();
     }
 }
