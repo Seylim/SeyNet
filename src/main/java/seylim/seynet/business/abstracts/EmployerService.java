305fc2bd3776ff2bc27d6dc1c20575613c2043ab
Emre Kaan Seylim <seylimemre@gmail.com>
2021-12-18 16:23:53 +0300

    register and login functions has been created for employer.

--- src/main/java/seylim/seynet/business/abstracts/EmployerService.java
+++ src/main/java/seylim/seynet/business/abstracts/EmployerService.java
@@ -1,6 +1,10 @@
 package seylim.seynet.business.abstracts;
 
+import seylim.seynet.core.utilities.Result;
+import seylim.seynet.entities.concretes.Candidate;
 import seylim.seynet.entities.concretes.Employer;
 
 public interface EmployerService extends BaseEntityService<Employer> {
+    Result register(Employer employer) throws Exception;
+    Result login(String eMail, String passwordAgain) throws Exception;
 }
