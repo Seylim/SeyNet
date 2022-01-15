6c2b0adc076c544990d100b7b5432cc1b444c8b3
Emre Kaan Seylim <seylimemre@gmail.com>
2022-01-15 11:53:35 +0300

    SocialMediaName class is created.

---
+++ src/main/java/seylim/seynet/entities/concretes/SocialMediaName.java
@@ -0,0 +1,24 @@
+package seylim.seynet.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Table(name = "social_media_names")
+public class SocialMediaName {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+}
